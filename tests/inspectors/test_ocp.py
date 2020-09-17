b38c5cab8ff89637f2c9d9c8cb1d70638ef94824
Zack Cerza <zack@redhat.com>
2020-09-17 12:11:40 -0600

    test_inspect_yaml

--- tests/inspectors/test_ocp.py
+++ tests/inspectors/test_ocp.py
@@ -1,9 +1,75 @@
 import pytest
+import yaml
+
+from unittest.mock import mock_open, patch
 
 from must_triage.inspectors.ocp import OCP
 
 
 class TestOCP:
+    def teardown(self):
+        patch.stopall()
+
+    @pytest.mark.parametrize(
+        "obj",
+        [
+            (
+                yaml.scanner.ScannerError
+            ),
+            (
+                dict(kind='pod')
+            ),
+            (
+                dict(
+                    kind='podList',
+                    items=[
+                        dict(kind='pod'),
+                        dict(kind='pod'),
+                    ],
+                )
+            ),
+            (
+                dict(error='something')
+            ),
+        ]
+    )
+    def test_inspect_yaml(self, obj):
+        path = '/fake_path'
+        p_safe_load = patch('must_triage.inspectors.ocp.yaml.safe_load')
+        m_safe_load = p_safe_load.start()
+        p_operator_success = patch(
+            'must_triage.inspectors.ocp.OCP.operator_success')
+        m_operator_success = p_operator_success.start()
+        p_pod_ready = patch('must_triage.inspectors.ocp.OCP.pod_ready')
+        m_pod_ready = p_pod_ready.start()
+        m_open = mock_open()
+        with patch('must_triage.inspectors.ocp.open', m_open):
+            if isinstance(obj, dict):
+                m_safe_load.return_value = obj
+            else:
+                m_safe_load.side_effect = obj
+            result = OCP.inspect_yaml(path)
+        if not isinstance(obj, dict):
+            assert result[path] == ["Failed to parse YAML content"]
+            return
+        assert len(m_operator_success.call_args_list) == \
+            len(list(filter(lambda o: 'kind' in o, obj)))
+
+        def pods_count(obj):
+            kind = obj.get('kind', '').lower()
+            if kind == 'pod':
+                return 1
+            if kind == 'podlist':
+                return len(
+                    [o for o in obj['items'] if o['kind'].lower() == 'pod']
+                )
+            return 0
+
+        assert len(m_pod_ready.call_args_list) == pods_count(obj)
+
+        if 'error' in obj:
+            assert obj in result[path]
+
     @pytest.mark.parametrize(
         "obj,expected",
         [
