3d46013946d0dec64e9a2bb5a9e1fee444f01267
Zack Cerza <zack@redhat.com>
2020-09-17 14:04:36 -0600

    test_gather

--- tests/inspectors/test_base.py
+++ tests/inspectors/test_base.py
@@ -2,6 +2,7 @@ import logging
 import pytest
 
 from concurrent.futures import ProcessPoolExecutor
+from unittest.mock import patch
 
 from must_triage.inspectors.base import Inspector
 
@@ -17,6 +18,19 @@ class TestInspector:
         assert inspector.root == '/tmp/must_triage_tests'
         assert inspector.progress is False
         assert isinstance(inspector.log, logging.Logger)
+
+    def test_gather(self, inspector):
+        inspector.gather_types = dict(
+            jpg=dict(
+                match=None,
+                description='desc',
+            )
+        )
+        with patch('must_triage.inspectors.base.fs.find') as m_find:
+            m_find.return_value = 'path'
+            inspector.gather()
+        assert m_find.call_count == len(inspector.gather_types.keys())
+        assert len(inspector.gathered.keys()) == m_find.call_count
 
     @staticmethod
     def _dummy_inspector(path):
