4644cbe021897dea07c56a6327e0e7d5c4a8f6e9
Zack Cerza <zack@redhat.com>
2020-09-17 12:43:06 -0600

    test_ocs: test all of inspect_log

--- tests/inspectors/test_ocs.py
+++ tests/inspectors/test_ocs.py
@@ -1,9 +1,14 @@
 import pytest
 
+from unittest.mock import mock_open, patch
+
 from must_triage.inspectors.ocs import OCS
 
 
 class TestOCS:
+    def teardown(self):
+        patch.stopall()
+
     @pytest.mark.parametrize(
         "obj,expected",
         [
@@ -22,17 +27,34 @@ class TestOCS:
         assert (obj['status'] == 'HEALTH_OK') != (obj in result)
 
     @pytest.mark.parametrize(
-        "line,expected",
+        "lines,panics",
         [
             (
-                'yesterday I Observed a panic: here it is',
-                True,
+                [
+                    'we Observed a panic: here it is',
+                ],
+                1,
             ),
             (
-                'tomorrow there will be no such panic',
-                False,
-            )
+                [
+                    'no panic',
+                ],
+                0,
+            ),
+            (
+                [
+                    'we Observed a panic: here it is',
+                    'no panic',
+                    'Observed a panic:',
+                ],
+                2,
+            ),
         ]
     )
-    def test_panicked(self, line, expected):
-        assert OCS.panicked(line) is expected
+    def test_inspect_log(self, lines, panics):
+        path = '/fake_path'
+        m_open = mock_open()
+        with patch('must_triage.inspectors.ocs.open', m_open):
+            m_open.return_value.readlines.return_value = lines
+            result = OCS.inspect_log(path)
+        assert len(result[path]) == panics
