c0da6c0903aa6d291cac47cc9fc537db92efdb17
Hiro Miyamoto <miyamotoh@us.ibm.com>
2020-12-01 20:07:43 -0500

    testing test code for `restart_count` func in CI
    Signed-off-by: Hiro Miyamoto <miyamotoh@us.ibm.com>

--- tests/inspectors/test_ocp.py
+++ tests/inspectors/test_ocp.py
@@ -151,3 +151,60 @@ class TestOCP:
     def test_pod_ready(self, obj, expected):
         result = OCP.pod_ready(obj)
         assert result == expected
+        
+    @pytest.mark.parametrize(
+        "obj,expected",
+        [
+            (
+                dict(
+                    metadata=dict(name='test_pod'),
+                    status=dict(containerStatuses=[
+                        dict(
+                            name='test_container',
+                            ready=False,
+                            state=dict(
+                                terminated=dict(
+                                    reason='garbage',
+                                ),
+                            ),
+                            restartCount=0,
+                        ),
+                    ]),
+                ),
+                ["Container 'test_container' in pod 'test_pod' has restart count"],
+            ),
+            (
+                dict(
+                    metadata=dict(name='test_pod'),
+                    status=dict(containerStatuses=[
+                        dict(
+                            name='test_container',
+                            ready=False,
+                            state=dict(
+                                terminated=dict(
+                                    reason='completed',
+                                    exitCode=0,
+                                ),
+                            ),
+                            restartCount=1,
+                        ),
+                    ]),
+                ),
+                list(),
+            ),
+            (
+                dict(
+                    metadata=dict(name='test_pod'),
+                    status=dict(),
+                ),
+                [dict(
+                    pod_name='test_pod',
+                    status=dict(),
+                )],
+            ),
+        ]
+    )    
+
+    def test_restart_count(self, obj, expected):
+        result = OCP.restart_count(obj)
+        assert result == expected
